37009c5ce9d99429d647bb77ef612ed8b2af22bd
An :) <tdinhan999@gmail.com>
2025-01-02 21:19:19 +0700

    Procastinate :(

--- pages/craps.py
+++ pages/craps.py
@@ -2,7 +2,7 @@ from random import randint
 from enum import Enum
 KEY_MAP_DISPLAY_TABLE = [0,0,0,0,0,0,0,0,0]
 BOARD_DIV = { 'current_phases_index': 0, 'roll_value': (0,0,0) }
-ACTIVE_BETS = {}
+ACTIVE_BETS = { 'pass_line':-1, 'come':[], 'place_bet':[], 'lay_bet':[], 'field_bet':False, 'big_6':False, 'big_8':False, 'any_7':False, 'hardways':0, 'bet_11':False, 'bet_3':False, 'bet_12':False }
 def _start():
     pass
 def _update():
@@ -14,4 +14,4 @@ def check_bet():
     global BOARD_DIV, ACTIVE_BETS
     if BOARD_DIV['current_phases_index'] == 0:
         if BOARD_DIV['roll_value'][0] == 7 or BOARD_DIV['roll_value'][0] == 11:
-            
+            pass
